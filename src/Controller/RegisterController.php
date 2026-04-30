17fed5ae0df2cebd71f43d6238d683859b89d085
Brahim Zeryouh <b.zeryouh@gmail.com>
2026-04-30 15:59:02 +0100

    etape 04

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,34 @@
+<?php
+// src/Controller/RegisterController.php
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegisterType;
+use App\Service\UserService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RegisterController extends AbstractController
+{
+    public function __construct(private UserService $userService) {}
+
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plainPassword = $form->get('plainPassword')->getData();
+            $this->userService->register($user, $plainPassword);
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('register/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Service/UserService.php
@@ -0,0 +1,26 @@
+<?php
+
+// src/Service/UserService.php
+namespace App\Service;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserService
+{
+    public function __construct(
+        private EntityManagerInterface      $em,
+        private UserPasswordHasherInterface $hasher
+    )
+    {
+    }
+
+    public function register(User $user, string $plainPassword): void
+    {
+        $hashed = $this->hasher->hashPassword($user, $plainPassword);
+        $user->setPassword($hashed);
+        $this->em->persist($user);
+        $this->em->flush();
+    }
+}
