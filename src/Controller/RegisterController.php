e265c0b953d9dccef8bae35cf776a01a822b7e4c
brahimzeryouh <b.zeryouh@gmail.com>
2026-05-25 23:40:43 +0100

    update

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,9 +2,9 @@
 // src/Controller/RegisterController.php
 namespace App\Controller;
 
-//use App\Entity\User;
+use App\Entity\User;
 use App\Form\RegisterType;
-//use App\Service\UserService;
+use App\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
